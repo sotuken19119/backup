7321fe88a3784f525f0643843a1db7de3ebee6ad
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-11-09 14:28:16 +0900

    Update Board.js

--- MineSweeper/component/Board.js
+++ MineSweeper/component/Board.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import CreateBoard from "../util/CreateBoard";
 
-const Board = () => {
-    return <h1>Board</h1>
+const Board = () =>{
+    //↓フック: const [オブジェクト, オブジェクトを変更する時呼び出すメソッド] = useState(変数の初期値)
+    //         メリットクラスを書かずにstateを利用できる
+    const [grid, setGrid] = useState([]);
+
+    useEffect(() => {
+        function freshBoard(){
+            const newBoard = CreateBoard(5, 5, 10);
+            setGrid(newBoard);
+        }
+        freshBoard();
+    }, []);
+    
+    //エラー処理
+    //if(!grid.board){
+    //    return <div>Loading</div>;
+    //}
+
+    //
+    return grid.board.map((singleRow) => {
+        return (
+            <div style={{display: "flex"}}>
+                {singleRow.map((singleBlock) => {
+                    return <div>{singleBlock.value}</div>;
+                })}
+            </div>
+        );
+    });
 };
 
 export default Board;
