04e8ed6cf480c16c9325aedaea283582a419e6de
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-11-27 15:04:20 +0900

    Update Modal.js

--- MineSweeper/component/Modal.js
+++ MineSweeper/component/Modal.js
@@ -1,27 +1,38 @@
 import React, { useState, useEffect } from "react";
 
-export default function Modal({ restartGame }) {
+export default function Modal({ reset }) {
+
+　//
   const [render, setRender] = useState(false);
+
   useEffect(() => {
-    setTimeout(() => {  // 時間を止める
-      setRender(true);
-    }, 1000);
+        //時間を止める
+        setTimeout(() => {  
+        
+            //ゲームの終わりを知らせる
+            setRender(true);
+        
+        }, 1000);
   }, []);
+
   return (
     <div
-    　// モーダル画面の表示
-      style={{
-        opacity: render ? 1 : 0,
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        background: "rgba(0,0,0,0.3)",
-      }}
+        // モーダルのデザイン
+        style={{
+            opacity: render ? 1 : 0,　//render-stateがtrueだったら透明化解除
+            height: "100%",
+            width: "100%",
+            position: "absolute",
+            background: "rgba(0,0,0,0.3)", //alphaは透明度の指定
+         }}
     >
-      <div id="gameOverImage"></div>
-      <div className="tryAgain" onClick={() => restartGame()}>
-        Try Again
-      </div>
+
+        <div id="gameOverImage"></div>
+
+        {/* 　リトライボタン */}
+        <div className="tryAgain" onClick={() => reset()}>
+            Try Again
+        </div>
     </div>
   );
 }
