0a3eb3c1654cd71ce2d67b8bc19a3c002da25353
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-11-27 15:37:55 +0900

    Add files via upload

---
+++ MineSweeper/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './CSS/App.css';
+import Board from './Components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MineSweepr</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
