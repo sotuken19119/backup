e5f1f46849da8a10673a42362a98804c5768bd70
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-12-11 13:25:40 +0900

    Add files via upload

--- MineSweeper/component/Timer.js
+++ MineSweeper/component/Timer.js
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from "react";
 import ModalClear from './ModalClear';
 let timeIntervalId; 
-export default function Timer({ gameOver, gameClear, sendTime ,setRestart}) {
+export default function Timer({ gameOver, gameClear,setGameClear, sendTime , setOptionflg , optionflg, restartflg, setRestartFlg}) {
   let [time, setTime] = useState(0);
   let [sTime, setSTime] = useState(0);
 
   const checkGame = ()=>{
-    if(gameClear == true || gameOver == true) return true;
+    if(gameClear == true || gameOver == true || optionflg || restartflg) return true;
     else return false;
   };
 
   useEffect(() => {
     // ゲームオーバーの処理
-    if ((time > 0 && gameOver) || (time > 0 && gameClear)) {
+    if ((time > 0 && gameOver) || (time > 0 && gameClear) || (time > 0 && optionflg) || (time > 0 && restartflg)) {
       setSTime(time); // ゲームオーバー時のタイム
-      setTime(0);     // タイマーを0にリセット
+      //setTime(0);
     }
-  }, [gameOver, gameClear ,time]);// 最初のマウント（DOMへのノードの追加)の時と指定された値に変化があった場合のみに第１引数の関数を実行。
+  }, [gameOver, gameClear , time , optionflg, restartflg] );// 最初のマウント（DOMへのノードの追加)の時と指定された値に変化があった場合のみに第１引数の関数を実行。
 
   // useEffectのコールバックが実行開始
   useEffect(() => {
     const incrementTime = () => {
         let newTime = time + 1; // タイムに1秒追加
         setTime(newTime); // タイムの更新
+        
     }; 
     // 1秒ごとの処理
     timeIntervalId = setTimeout(() => {
-        incrementTime();  
+        incrementTime(); 
     },1000);
-    if(gameOver || gameClear) {
-    clearInterval(timeIntervalId);　// ここでクリアすることで多重にタイマーが実行されないようにする
+    if(gameOver || gameClear || optionflg || restartflg) {
+      setOptionflg(false);
+      setRestartFlg(false);
+      clearInterval(timeIntervalId);　// ここでクリアすることで多重にタイマーが実行されないようにする
+      setTime(0);     // タイマーを0にリセット
+      
     }
   }, [time, setTime, gameOver,gameClear, sendTime]);// 最初のマウント（DOMへのノードの追加)の時と指定された値に変化があった場合のみに第１引数の関数を実行。
 
@@ -42,7 +47,7 @@ export default function Timer({ gameOver, gameClear, sendTime ,setRestart}) {
         ⏰
       </span>
       {checkGame() ? sTime : time}　{/* ゲームオーバーなら終了時のタイムを表示、違うならタイムを表示 */}
-      {gameClear && <ModalClear reset={setRestart} completeTime={sTime} />}
+      {gameClear && <ModalClear completeTime={sTime} setGameClear={setGameClear} />}
     </div>
   );
 }
