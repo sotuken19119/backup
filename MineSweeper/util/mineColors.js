14510e27346d2947e5f8c8505d432f9690e15827
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-12-21 14:21:43 +0900

    Delete mineColors.js

--- MineSweeper/util/mineColors.js
+++
@@ -1,5 +0,0 @@
-export const mineColor = () => {
-    // ランダムに爆弾の色を決めている
-    let colors = ["orange", "darkgreen", "cyan", "violet", "yellow"];
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
