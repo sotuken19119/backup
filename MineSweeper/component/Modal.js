52c3f6084e199798ebc0e3878aafc4171475a296
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-12-21 13:31:30 +0900

    Update Modal.js

--- MineSweeper/component/Modal.js
+++ MineSweeper/component/Modal.js
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from "react";
 
-export default function Modal({setGameOver }) {
+export default function Modal({ reset }) {
+
   const [render, setRender] = useState(false);
+
   useEffect(() => {
-    setTimeout(() => {  // 時間を止める
-      setRender(true);
-    }, 1000);
+        //時間を止める
+        setTimeout(() => { 
+            //ゲームの終わりを知らせる
+            setRender(true);
+        }, 1000);
   }, []);
-  function reset(){
-    setRender(false);
-    setGameOver(false);
-  }
+
   return (
     <div
-    　// モーダル画面の表示
-      style={{
-        opacity: render ? 1 : 0,
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        background: "rgba(0,0,0,0.3)",
-      }}
+        // モーダルのデザイン
+        style={{
+            opacity: render ? 1 : 0,　//render-stateがtrueだったら透明化解除
+            height: "160%",
+            width: "150%",
+            marginTop: -150,
+            position: "absolute",
+            background: "rgba(0,0,0,0.3)", //alphaは透明度の指定
+            zIndex: 9999
+         }}
     >
-      {/* ゲームオーバーの時の画面 */}
-      <div id="gameOverImage"></div>
-      <div onClick={() => reset()} className="tryAgain">
-        もう一度
+        {/* ゲームオーバーの時の画面 */}
+      <div className="textGameOver">
+        
+      <br/>
+      <br/>
+        GAME OVER
+        
       </div>
+
+        {/* 　リトライボタン */}
+        <div className="tryAgain" onClick={() => reset()}>
+            Try Again
+        </div>
+
     </div>
   );
 }
