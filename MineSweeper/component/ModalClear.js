908ee0aa1ff67f612128437e9fad9793e8432d4b
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-12-21 13:31:51 +0900

    Update ModalClear.js

--- MineSweeper/component/ModalClear.js
+++ MineSweeper/component/ModalClear.js
@@ -1,34 +1,37 @@
 import React, { useState, useEffect } from "react";
 
-export default function ModalClear({ completeTime,setGameClear}) {
+export default function ModalClear({ reset, completeTime }) {
   const [render, setRender] = useState(false);
   useEffect(() => {
     setTimeout(() => {  // 時間を止める
       setRender(true);
     }, 1000);
   }, []);
-
-  function reset(){
-    setRender(false);
-    setGameClear(false);
-  }
   return (
     <div
     　// モーダル画面の表示
       style={{
         opacity: render ? 1 : 0,
-        height: "100%",
+        height: "160%",
         width: "100%",
+        marginTop: -150,
         position: "absolute",
-        background: "rgba(0,0,0,0.3)",
+        zIndex: 9999
       }}
     >
-      {/* ゲームオーバーの時の画面 */}
-      <div id="gameOverImage"><br/><br/>クリア</div>
-      <div>クリアタイム:{completeTime}</div>
-      <div onClick={() => reset()} className="tryAgain">
-        ホームに戻る
+      {/* クリア時の画面 */}
+      <div className="textClear">
+
+        <br/>
+        <br/>
+          Congratulation!!
+        </div>
+
+      {/* 　リトライボタン */}
+      <div className="tryAgain" onClick={() => reset()}>
+            Try Again
       </div>
+
     </div>
   );
 }
