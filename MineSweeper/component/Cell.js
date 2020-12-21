b430f5a53d9902226fef35e3313f33eea655b532
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-12-21 13:30:52 +0900

    Update Cell.js

--- MineSweeper/component/Cell.js
+++ MineSweeper/component/Cell.js
@@ -1,104 +1,76 @@
-import React from "react";
-import { mineColor } from "../util/mineColors";
-import Circle from "./Circle";
+import React from 'react';
+import '../CSS/App.css';
 
-export default function Cell({ data, updateBoard, flagCell, flagDei,sumFlag}) {
-  const style = {
-    block: {
-      width: 40,
-      height: 40,
-      color: numColorCode(data.value),
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontWeight: 800,
-      fontSize: 30,
-      cursor: "pointer",
-      background: data.revealed
-        ? data.value === "X"
-          ? mineColor()
-          : bombChexPattern(data.x, data.y)
-        : chexPattern(data.x, data.y),
-    },
-  };
+export default function Cell({ details, revealCell, flagCell, flagDei, sumFlag}) {
 
-  const onClickUpdate = (e) => {
-    if (data.flagged) {
-      return;
-    }
-    
-    console.log(e.type);
-    updateBoard(data.x, data.y,e);
-    sumFlag();              
-  };
+    const cellstyle = {
+        background: details.Revealed
+            ? details.value === "X"
+                ? "#f2d5a3"
+                :bombChexPattern(details.x, details.y)
+            : chexPattern(details.x, details.y),
+        color:numColorCode(details.value),
+    };
 
-  const onClickFlag = (e) => {   
-    e.preventDefault();
-    if(!data.revealed){
-      flagCell(data.x, data.y); 
-      flagDei(data.x, data.y);        
-    }
-  };
+    const onClickUpdate = (e) => {
+        if(details.flagged){
+            return;
+        }
 
-  return (
-    <div
-      style={style.block}
-      onClick={(e) => onClickUpdate(e)}
-      onContextMenu={(e) => onClickFlag(e)}     
-    >
-      {data.flagged && !data.revealed ? (
-            "🚩"
-      ) : data.revealed && data.value !== 0 ? (
-        data.value === "X" ? (
-          <Circle />
-        ) : (
-          data.value
-        )
-      ) : (
-        ""
-      )}
-    </div>
-  );
+        console.log(e.type);
+        revealCell(details.x, details.y);
+        sumFlag();
+    };
+
+    const onClickFlag = (e) => {
+        e.preventDefault();
+        if(!details.revealed){
+            flagCell(e, details.x, details.y);
+            flagDei(details.x, details.y);
+        }
+    };
+
+    return (
+        <div 
+            onContextMenu={(e) => onClickFlag(e, details.x, details.y)}
+            onClick = {() => onClickUpdate(details.x, details.y)} 
+            style = {cellstyle}
+            className = "cellStyle"
+        >
+            {!details.Revealed && details.flagged
+                ? "🚩" 
+                    : details.Revealed && details.value !== 0 
+                    ? (details.value === "X" ? "💣" : details.value) 
+                : ("")}
+        </div>
+    );
 }
 
-const chexPattern = (x, y) => {
-  if (x % 2 === 0 && y % 2 === 0) {
-    return "#aad751";
-  } else if (x % 2 === 0 && y % 2 !== 0) {
-    return "#a2d249";
-  } else if (x % 2 !== 0 && y % 2 === 0) {
-    return "#a2d249";
-  } else {
-    return "#aad751";
-  }
-};
+
+
 
 const bombChexPattern = (x, y) => {
-  if (x % 2 === 0 && y % 2 === 0) {
-    return "#e5c29f";
-  } else if (x % 2 === 0 && y % 2 !== 0) {
-    return "#d7b899";
-  } else if (x % 2 !== 0 && y % 2 === 0) {
-    return "#d7b899";
-  } else {
-    return "#e5c29f";
-  }
-};
+    return "#f2d5a3";
+    };
+    
+const chexPattern = (x, y) => {
+    };
 
 const numColorCode = (num) => {
-  if (num === 1) {
-    return "#1976d2";
-  } else if (num === 2) {
-    return "#388d3c";
-  } else if (num === 3) {
-    return "#d33030";
-  } else if (num === 4) {
-    return "#7c21a2";
-  } else if (num === 5) {
-    return "#1976d2";
-  } else if (num === 6) {
-    return "#1976d2";
-  } else {
-    return "white";
-  }
+    if (num === 1) {
+        return "#1976d2";
+    } else if (num === 2) {
+        return "#388d3c";
+    } else if (num === 3) {
+        return "#d33030";
+    } else if (num === 4) {
+        return "#7c21a2";
+    } else if (num === 5) {
+        return "#1976d2";
+    } else if (num === 6) {
+        return "#1976d2";
+    } else {
+        return "white";
+    }
 };
+
